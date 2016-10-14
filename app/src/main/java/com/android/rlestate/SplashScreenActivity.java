04e37cb3cd76b911e330ed498e3e5770b18cebd0
bitfield66 <vinayyam@gmail.com>
2016-10-14 22:11:45 +0530

    changes

---
+++ app/src/main/java/com/android/rlestate/SplashScreenActivity.java
@@ -0,0 +1,12 @@
+package com.android.rlestate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends BaseActivity {
+
+    @Override
+    public void initial() {
+
+    }
+}
